0f1fa837a4d39c7f776ac1ae9abd9cf549959824
Igor Zelenov <igorg.zelenov@gmail.com>
2020-01-31 09:16:22 -0500

    new commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,40 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+document.body.querySelector('#start-button').onclick = function() {
+  this.remove()
+  startGame()
+}
 
-  function startGame() {
+const canvas = document.querySelector('#canvas');
+canvas.width = window.innerWidth/1.5;
+canvas.height = 500
+const ctx = canvas.getContext('2d');
 
+function startGame() {
+  console.log('START')
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0,0,canvas.width, canvas.height)
+
+  ctx.fillStyle = 'grey'
+  ctx.fillRect(100,0,canvas.width-200, canvas.height)
+}
+
+function draw(car) {
+  var img = new Image();
+  img.onload = function() {
+    ctx.drawImage(img, car.x, car.y, 50, 50);
   }
-};
+  // img.scr = ".../link to the car.png/"
+}
+
+function animate() {
+  let loop = window.requestAnimationFrame(animate)
+}
+
+// window.onload = function() {
+//   document.getElementById("start-button").onclick = function() {
+//     startGame();
+//   };
+
+//   function startGame() {
+
+//   }
+// };
